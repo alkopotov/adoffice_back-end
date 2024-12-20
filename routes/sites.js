b4298208cd4193f57685304ccb4481ce7765c347
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-12-20 17:39:13 +0300

    Getting Site with adunits

--- routes/sites.js
+++ routes/sites.js
@@ -3,11 +3,14 @@ const Category = require('../database/models/category');
 const Seasonal = require('../database/models/seasonal');
 const Adunit = require('../database/models/adunit');
 const Discount = require('../database/models/discount');
+const Adformat = require('../database/models/adformat');
+
 
 // const { request } = require('express');
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const User = require('../database/models/user');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -27,7 +30,7 @@ router.get('/all', (req, res) => {
 router.get('/:id', (req, res) => {
   Site.findByPk(
     req.params.id,
-    {include: [Category, Seasonal, Adunit, Discount]}
+    {include: [Category, Seasonal, {model: Adunit, iclude: Adformat}, Discount]}
   )
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
@@ -35,9 +38,30 @@ router.get('/:id', (req, res) => {
 
 router.post('/', jsonParser, (req, res) => {
   console.log(JSON.stringify(req.body));
-  Site.create(req.body)
-    .then((data) => res.json(data))
-    .catch((err) => res.json(err));
+
+  User.findOne({
+    where: {
+      user_token: req.body.user_token || ''
+    }
+  })
+  .then((user) => {    
+    if (user) {
+      Site.create({
+        site_name: req.body.site_name,
+        site_url: req.body.site_url,
+        site_cover_daily: req.body.site_cover_daily,
+        site_cover_weekly: req.body.site_cover_weekly,
+        site_cover_monthly: req.body.site_cover_monthly,
+        categoryIdCategory: req.body.categoryIdCategory,
+        userIdUser: user.id_user
+      })
+        .then((data) => res.json(data))
+        .catch((err) => res.json(err));
+    } else {     
+      res.status(401).json({ auth_error: 'Неверный токен' });
+    }
+  })
+  .catch((err) => res.json(err));
 });
 
 
