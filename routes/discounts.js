77e944a10a86a777fab0609c46b2c0ec685053bf
Alexey Kopotov <alexey@iMac-Alexey.local>
2025-01-07 14:00:12 +0300

    Add Discount Route

--- routes/discounts.js
+++ routes/discounts.js
@@ -1,4 +1,6 @@
 const Discount = require('../database/models/discount');
+const User = require('../database/models/user');
+const Site = require('../database/models/site');
 
 const express = require('express');
 const router = express.Router();
@@ -18,4 +20,40 @@ router.get('/:id', (req, res) => {
     .catch((err) => res.json(err));
 })
 
+
+router.post('/', jsonParser, (req, res) => {
+  User.findOne({
+    where: {
+      user_token: req.body.user_token || ''
+    }
+  }).
+    then((user) => {
+      if (user) {
+        if(user.is_super){
+          Discount.create(req.body)
+            .then((data) => res.json(data))
+            .catch((err) => res.json(err));
+        }
+        else {
+          Site.findOne({
+            where: {
+              id_site: req.body.siteIdSite,
+              userIdUser: user.id_user
+            }
+          }).
+            then((site) => {
+              if (site) {
+                Discount.create(req.body)
+                  .then((data) => res.json(data))
+                  .catch((err) => res.json(err));
+              }
+              else res.status(401).json({ auth_error: 'Неверный токен' });
+            })
+            .catch((err) => res.json(err));
+        }
+      }
+      else res.status(401).json({ auth_error: 'Неверный токен' });
+    })
+    .catch((err) => res.json(err));
+})
 module.exports = router;
