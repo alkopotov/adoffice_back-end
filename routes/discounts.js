15653edcc3912e0409365c8cfb5183f689ea4163
Alexey Kopotov <alexey@iMac-Alexey.local>
2025-01-08 18:00:27 +0300

    Discount delete added

--- routes/discounts.js
+++ routes/discounts.js
@@ -10,17 +10,6 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 
-router.get('/:id', (req, res) => {  
-  Discount.findAll(  {
-    where: {
-      siteIdSite: req.params.id
-    }
-  })
-    .then((data) => res.json(data))
-    .catch((err) => res.json(err));
-})
-
-
 router.post('/', jsonParser, (req, res) => {
   User.findOne({
     where: {
@@ -54,6 +43,63 @@ router.post('/', jsonParser, (req, res) => {
       }
       else res.status(401).json({ auth_error: 'Неверный токен' });
     })
+    .catch((err) => res.json(err));
+})
+
+router.delete('/delete', jsonParser, (req, res) => {
+  User.findOne({
+    where: {
+      user_token: req.body.user_token || ''
+    }
+  }).
+    then((user) => {
+      if (user) {
+        if(user.is_super){
+          Discount.destroy({
+            where: {
+              id_discount: req.body.id_discount
+            }
+          })
+            .then((data) => res.json(data))
+            .catch((err) => res.json(err));
+        }
+        else {
+          Site.findOne({
+            where: {
+              id_site: req.body.siteIdSite,
+              userIdUser: user.id_user
+            }
+          }).
+            then((site) => {
+              if (site) {
+                Discount.destroy({
+                  where: {
+                    id_discount: req.body.id_discount
+                  }
+                })
+                  .then((data) => res.json(data))
+                  .catch((err) => res.json(err));
+              }
+              else res.status(401).json({ auth_error: 'Неверный токен' });
+            })
+            .catch((err) => res.json(err));
+        }
+      }
+      else {
+        res.status(401).json({ auth_error: 'Неверный токен' });
+      }
+    
+  }).
+    catch((err) => res.json(err));
+})
+
+router.get('/:id', (req, res) => {  
+  Discount.findAll(  {
+    where: {
+      siteIdSite: req.params.id
+    }
+  })
+    .then((data) => res.json(data))
     .catch((err) => res.json(err));
 })
 module.exports = router;
